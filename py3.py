bebfd7655c6741102a905ac7e20de2cf604fef56
ny-yohei <yo0314baseball@gmail.com>
2021-06-11 20:40:39 +0900

    編集

--- py3.py
+++ py3.py
@@ -17,13 +17,13 @@ import math
 def get_num(value):
   if value <0:
     return []
-  prime[2]
+  prime=[2]
   limit =int(math.sqrt(value))
   
-  data =[i +1 for i in range(2,value.2)]
+  data =[i +1 for i in range(2,value,2)]
   while limit >data[0]:
     prime.append(data[0])
     data =[j for j in data if j %data[0]!=0]
-   return prime + data
+  return prime + data
 
-print(get_num(200)
+print(get_num(200))
