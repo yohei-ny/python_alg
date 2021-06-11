090793a8ba4943d381ae21460d890eebfd7a80d4
yohei-ny <60955704+yohei-ny@users.noreply.github.com>
2021-06-11 20:36:37 +0900

    Update py3.py

--- py3.py
+++ py3.py
@@ -1,13 +1,29 @@
+# import math
+
+# def get_num(not_value):
+#   if not_value <=1:
+#     return False
+#   for i in range(2,int(math.sqrt(not_value))+1):
+#     if not_value % i ==0:
+#       return False
+#   return True
+
+# for i in range(200):
+#   if get_num(i):
+#     print(i)
+
 import math
 
-def get_num(not_value):
-  if not_value <=1:
-    return False
-  for i in range(2,int(math.sqrt(not_value))+1):
-    if not_value % i ==0:
-      return False
-  return True
+def get_num(value):
+  if value <0:
+    return []
+  prime[2]
+  limit =int(math.sqrt(value))
+  
+  data =[i +1 for i in range(2,value.2)]
+  while limit >data[0]:
+    prime.append(data[0])
+    data =[j for j in data if j %data[0]!=0]
+   return prime + data
 
-for i in range(200):
-  if get_num(i):
-    print(i)
+print(get_num(200)
