090d4e786b772b9cc5c282c56b306fc0949c0137
ny-yohei <yo0314baseball@gmail.com>
2021-06-14 20:10:02 +0900

    pra4

--- pra4.py
+++ pra4.py
@@ -1,12 +1,23 @@
-memo ={1:1,2:2}
-def fibonachi(n):
-    if n in memo:
-        return memo[n]
+#memo ={1:1,2:2}
+#def fibonachi(n):
+#    if n in memo:
+#        return memo[n]
    # if (n==1)or(n==2):
     #    return 1
     #return fibonachi(n-2)+ fibonachi(n-1)
-    memo[n] =fibonachi(n-2)+ fibonachi(n-1)
-    return memo[n]
-num =int(input())
-print(fibonachi(num))
+#    memo[n] =fibonachi(n-2)+ fibonachi(n-1)
+#    return memo[n]
+#num i=int(input())
+#print(fibonachi(num))
 ##practice
+
+
+def fibonachi(n):
+    fib =[1,1]
+    for i in range(2,n):
+        fib.append(fib[i-2]+fib[i-1]
+    return fib[n-1]
+
+num i=int(input())
+print(fibonachi(num))
+
