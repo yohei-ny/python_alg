459222e695599d4c4d66bf95f2969ad727eb0ed5
ny-yohei <yo0314baseball@gmail.com>
2021-07-05 18:55:22 +0900

    python

--- multi2.py
+++ multi2.py
@@ -1,5 +1,5 @@
 n =10
 for i in range(1,n):
   for j in range(1,n):
-    
-    print(str(i)+"*" +str(j) + "=" +str(i*j))      
+    for k in range(1,n):
+        print(str(i)+ "*" +str(j) + "*" + str(k) +"=" +str(i*j*k)) 
