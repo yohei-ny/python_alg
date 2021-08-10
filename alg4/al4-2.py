88737c3af1581741a39e5dc028fd237a0323fad6
ny-yohei <yo0314baseball@gmail.com>
2021-08-10 13:53:58 +0900

    al4

--- alg4/al4-2.py
+++ alg4/al4-2.py
@@ -1,2 +1,9 @@
 def linear_serach(data,value):
   for i in range(len(data)):
+    if data[i] == value:
+      return i
+  return -1
+
+data =[50,30,90,10,20,70,60,40,80]
+print(linear_serach(data,40))
+          
