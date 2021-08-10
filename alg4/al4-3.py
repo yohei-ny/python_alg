648236d89b4a8a8ff2f48123edc8f4f816adbd00
ny-yohei <yo0314baseball@gmail.com>
2021-08-10 14:00:37 +0900

    al4

---
+++ alg4/al4-3.py
@@ -0,0 +1,16 @@
+def binary_search(data,value):
+  left = 0
+  right = len(data) - 1
+  while left <= right:
+    mid =(left + right) // 2
+    if data[mid] == value:
+      return mid
+    elif data[mid] < value:
+      left = mid + 1
+    else:
+      right = mid - 1
+  return -1       
+
+data = [10,20,30,40,50,60,70,80,90]
+
+print(data,90)
