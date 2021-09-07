805aa04791e84d85b5362329508179dbe885b517
ny-yohei <yo0314baseball@gmail.com>
2021-09-07 11:55:40 +0900

    test

---
+++ line_apy.py
@@ -0,0 +1,11 @@
+import os
+
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn)
